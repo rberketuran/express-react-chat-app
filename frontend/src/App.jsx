d3ed8aa9c3db90e9be783427683872350e9a1a17
dusty0ne <z3r0.dusty@gmail.com>
2024-07-11 14:47:03 +0300

    sign up and home page updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
-import Login from './pages/login/Login';
+//import Login from './pages/login/Login';
+//import SignUp from './pages/signup/SignUp';
+import Home from './pages/home/Home';
+
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Login />
+      <Home />
     </div>
   )
 }
